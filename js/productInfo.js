c272210faaa57ad70f81a32c67cfffdf0f9554e2
Ilaha Hasanli <hasanliilaha12@gmail.com>
2023-12-01 21:16:14 +0400

    display productInfo

--- js/productInfo.js
+++ js/productInfo.js
@@ -3,14 +3,14 @@ let productDetail = document.querySelector(".productDetails");
 let id = new URLSearchParams(window.location.search).get('id');
 
 // Function to fetch product details by ID asynchronously
-async function getProductsById(id) {
+async function getProductDetailsWithId(id) {
     return (
         // Fetch data from the URL constructed with the ID parameter
         fetch(`${'https://dummyjson.com/products'}/${id}`)
             .then(response => {
                 if (!response.ok) {
-                    // Throw an error if the network response is not okay
-                    throw new Error('Network response was not ok.');
+                    // an error if the network response is not okay
+                    return 'Network response was not ok.';
                 }
                 return response.json();
             })
@@ -20,12 +20,31 @@ async function getProductsById(id) {
             }));
 }
 
-getProductsById(id)
-    .then(product => {
-        if (product) {
-            console.log(product); 
-        } else {
-            console.log('Product not found.');
-        }
-    });
+
+function displayProductDetails() {
+    getProductDetailsWithId(id)
+        .then(product => {
+            if (product) {
+                const productItem = `
+      <div class="product-item">
+        <img class="product-image" src="${product.thumbnail}" alt="${product.title}">
+        <h2 class="product-title">${product.title}</h2>
+        <div class="texts">
+          <p class="product-category">${product.category}</p>
+          <p class="product-description">${product.description}</p>
+          <span class="product-price">Price: $${product.price}</span>F
+          <p class="product-discount">Discount: ${product.discount}%</p>
+          <p class="product-stock">Stocks: ${product.stock}</p>
+        </div>
+      </div>
+    `;
+                productDetail.innerHTML = productItem;
+            }
+            else {
+                console.log('Product not found.');
+            }
+        });
+}
+
+displayProductDetails();
 
